5ce50f4790e86ec42019e014a1410b7c7b10fb36
franciscofunes <ffunes90@gmail.com>
2020-12-02 19:47:00 -0300

    create product component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 //feature 1
 import React from 'react';
+import data from './data.json';
+import Products from './components/Products';
 
-function App() {
-	return (
-		<div className='grid-container'>
-			<header>
-				<a href='/'>Carrito de Compras</a>
-			</header>
-			<main>Lista Productos</main>
-			<footer>Todos los derechos reservados.</footer>
-		</div>
-	);
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			products: data.products,
+			size: '',
+			sort: '',
+		};
+	}
+	render() {
+		return (
+			<div className='grid-container'>
+				<header>
+					<a href='/'>Carrito de Compras</a>
+				</header>
+				<main>
+					<div className='content'>
+						<div className='main'>productos</div>
+						<Products products={this.state.products}></Products>
+						<div className='sidebar'>items del carrito</div>
+					</div>
+				</main>
+				<footer>Todos los derechos reservados.</footer>
+			</div>
+		);
+	}
 }
 
 export default App;
